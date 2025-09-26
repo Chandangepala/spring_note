b5245f1fa802d3a3531c9bdd585d9dd16cde9d18
chandangepala <chandangepala@gmail.com>
2025-09-27 03:56:22 +0530

    insert note | fetch  notes

---
+++ src/main/kotlin/com/basic/spring_note/database/repositories/NoteRepository.kt
@@ -0,0 +1,9 @@
+package com.basic.spring_note.database.repositories
+
+import com.basic.spring_note.database.models.Note
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface NoteRepository: MongoRepository<Note, ObjectId> {
+    fun findByOwnerId(ownerId: ObjectId): List<Note>
+}
